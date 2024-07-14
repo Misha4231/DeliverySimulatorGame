87d8eaf5f90527bccc63697b2208bfeae8b4576b
Misha <mishagalko1@gmail.com>
2024-07-14 15:09:04 +0200

    Add bicycle source files

---
+++ Source/DeliverySimulator/Transport/Bicycle.cpp
@@ -0,0 +1,227 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bicycle.h"
+#include "ChaosVehicleMovementComponent.h"
+#include "../Character/ThirdPersonCharacter.h"
+#include "EventManager.h"
+
+FBikePoints::FBikePoints(const FVector& InSeat, const FVector& InLeftHand, const FVector& InRightHand, 
+						 const FVector& InLeftPedal, const FVector& InRightPedal)
+	: Seat(InSeat), LeftHand(InLeftHand), RightHand(InRightHand), LeftPedal(InLeftPedal), RightPedal(InRightPedal)
+{
+}
+
+
+ABicycle::ABicycle()
+{
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraBoom);
+
+	CharacterMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
+	CharacterMesh->SetupAttachment(RootComponent);
+
+	GetIntoBoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("GetIntoBox"));
+	GetIntoBoxCollision->SetupAttachment(RootComponent);
+	
+	GetMesh()->SetSimulatePhysics(true);
+}
+
+void ABicycle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetIntoBoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ABicycle::OnBoxOverlapBegin);
+	GetIntoBoxCollision->OnComponentEndOverlap.AddDynamic(this, &ABicycle::OnBoxOverlapEnd);
+}
+
+void ABicycle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Camera
+	PlayerInputComponent->BindAxis("LookRight", this, &ABicycle::LookRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &ABicycle::LookUp);
+
+	// Movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABicycle::MoveForward);
+	PlayerInputComponent->BindAxis("TurnLeft", this, &ABicycle::TurnLeft);
+	PlayerInputComponent->BindAxis("MoveBack", this, &ABicycle::MoveBack);
+
+	// Break
+	PlayerInputComponent->BindAction("HandBrake", EInputEvent::IE_Pressed, this, &ABicycle::StartHandBrake);
+	PlayerInputComponent->BindAction("HandBrake", EInputEvent::IE_Released, this, &ABicycle::StopHandBrake);
+
+	// Exit
+	PlayerInputComponent->BindAction("GetInside", IE_Pressed, this, &ABicycle::GetOutOfBicycle);
+}
+
+void ABicycle::LookRight(const float Scale)
+{
+	AddControllerYawInput(Scale);
+}
+
+void ABicycle::LookUp(const float Scale)
+{
+	AddControllerPitchInput(Scale);
+}
+
+void ABicycle::MoveForward(const float Scale)
+{
+	GetVehicleMovementComponent()->SetThrottleInput(Scale);
+}
+
+void ABicycle::TurnLeft(const float Scale)
+{
+	GetVehicleMovementComponent()->SetSteeringInput(Scale);
+}
+
+void ABicycle::MoveBack(const float Scale)
+{
+	GetVehicleMovementComponent()->SetBrakeInput(-Scale);
+}
+
+void ABicycle::StartHandBrake()
+{
+	GetVehicleMovementComponent()->SetHandbrakeInput(true);
+}
+void ABicycle::StopHandBrake()
+{
+	GetVehicleMovementComponent()->SetHandbrakeInput(false);
+}
+
+void ABicycle::GetOutOfBicycle()
+{
+	if (!GetOutOfBicycleMontage) return;
+	
+	StartHandBrake();
+	const bool bRightBody = SideRayCast(GetMesh()->GetRightVector());
+	const bool bLeftBody = SideRayCast(-GetMesh()->GetRightVector());
+
+	if (!bRightBody || !bLeftBody)
+	{
+		bMirrorAnimations = bRightBody;
+
+		UAnimInstance* AnimInstance = CharacterMesh->GetAnimInstance();
+		if (!AnimInstance->Montage_IsPlaying(GetOutOfBicycleMontage))
+		{
+			int32 const SectionIndex = GetOutOfBicycleMontage->GetSectionIndex("Default");
+			int32 const SectionLength = GetOutOfBicycleMontage->GetSectionLength(SectionIndex);
+			
+			AnimInstance->Montage_Play(GetOutOfBicycleMontage);
+			GetWorldTimerManager().SetTimer(GetOutOfBicycleHandle, [this, bRightBody]()
+			{
+				PossessCharacter(!bRightBody);
+			}, SectionLength, false);
+		}
+	}
+}
+
+float ABicycle::GetAcceleration() const
+{
+	if (InputComponent)
+		return InputComponent->GetAxisValue("MoveForward") + (InputComponent->GetAxisValue("MoveBack"));
+	return 0;
+}
+
+FBikePoints ABicycle::GetBikePoints() const
+{
+	// Get the mesh component
+	const USkeletalMeshComponent* MeshComponent = GetMesh();
+
+	// Ensure MeshComponent is valid
+	if (!MeshComponent)
+	{
+		return FBikePoints();
+	}
+
+	// Get the world locations of the sockets
+	FTransform SeatLocation = MeshComponent->GetSocketTransform("seat");
+	FTransform LeftHandLocation = MeshComponent->GetSocketTransform("left_hand");
+	FTransform RightHandLocation = MeshComponent->GetSocketTransform("right_hand");
+	FTransform LeftPedalLocation = MeshComponent->GetSocketTransform("penal_left");
+	FTransform RightPedalLocation = MeshComponent->GetSocketTransform("pedal_right");
+
+	FTransform CharacterWorldTransform = CharacterMesh->GetComponentTransform();
+
+	// Create FTransforms using the local locations
+	FTransform LocalSeatTransform = UKismetMathLibrary::MakeRelativeTransform(SeatLocation, CharacterWorldTransform);
+	FTransform LocalLeftHandTransform = UKismetMathLibrary::MakeRelativeTransform(LeftHandLocation, CharacterWorldTransform);
+	FTransform LocalRightHandTransform = UKismetMathLibrary::MakeRelativeTransform(RightHandLocation, CharacterWorldTransform);
+	FTransform LocalLeftPedalTransform = UKismetMathLibrary::MakeRelativeTransform(LeftPedalLocation, CharacterWorldTransform);
+	FTransform LocalRightPedalTransform = UKismetMathLibrary::MakeRelativeTransform(RightPedalLocation, CharacterWorldTransform);
+	
+	// Create and return FBikePoints with local transforms
+	FBikePoints Points = FBikePoints(
+		LocalSeatTransform.GetTranslation(),
+		LocalLeftHandTransform.GetTranslation(),
+		LocalRightHandTransform.GetTranslation(),
+		LocalLeftPedalTransform.GetTranslation(),
+		LocalRightPedalTransform.GetTranslation()
+	);
+
+	return Points;
+}
+
+void ABicycle::OnBoxOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (AThirdPersonCharacter* Character = Cast<AThirdPersonCharacter>(OtherActor))
+	{
+		Character->HandleEnterBicycleCollision(this);
+
+	}
+}
+
+void ABicycle::OnBoxOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (AThirdPersonCharacter* Character = Cast<AThirdPersonCharacter>(OtherActor))
+	{
+		Character->HandleExitBicycleCollision(this);
+	}
+}
+
+bool ABicycle::SideRayCast(const FVector& Direction) const
+{
+	FVector RayStart = GetMesh()->GetComponentLocation();
+	FVector RayEnd = RayStart + (Direction * 150.f);
+	
+	constexpr float VerticalOffset = 50.f;
+
+	RayStart.Z += VerticalOffset;
+	RayEnd.Z += VerticalOffset;
+
+	FHitResult hit;
+	if (GetWorld() &&
+		GetWorld()->LineTraceSingleByChannel(hit, RayStart, RayEnd, ECC_PhysicsBody))
+	{
+		if (hit.GetActor())
+		{
+			return true;
+		}
+	}
+	
+	//DrawDebugLine(GetWorld(), RayStart, RayEnd, FColor::Red, false, 5.f, 0, 5);
+
+	return false;
+}
+
+void ABicycle::PossessCharacter(const bool Side)
+{
+	const FVector PossessLocation = Side ?
+		GetMesh()->GetSocketLocation("get_from_right_socket") :
+		GetMesh()->GetSocketLocation("get_from_left_socket");
+	
+	CharacterMesh->SetVisibility(false);
+	
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	AThirdPersonCharacter* Character = GetWorld()->SpawnActor<AThirdPersonCharacter>(CharacterClass, PossessLocation, GetActorRotation(), SpawnParams);
+	if (!Character)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Blue, "not character");
+	}
+	GetController()->Possess(Character);
+}
---
+++ Source/DeliverySimulator/Transport/Bicycle.h
@@ -0,0 +1,125 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Bicycle.generated.h"
+
+class AThirdPersonCharacter;
+
+USTRUCT(BlueprintType)
+struct FBikePoints
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bike Points")
+	FVector Seat;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bike Points")
+	FVector LeftHand;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bike Points")
+	FVector RightHand;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bike Points")
+	FVector LeftPedal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bike Points")
+	FVector RightPedal;
+
+	FBikePoints() {}
+	
+	FBikePoints(const FVector& InSeat, const FVector& InLeftHand, const FVector& InRightHand, 
+						 const FVector& InLeftPedal, const FVector& InRightPedal);
+};
+
+/**
+ * 
+ */
+UCLASS()
+class DELIVERYSIMULATOR_API ABicycle : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+
+public:
+	//  Constructor
+	ABicycle();
+
+	virtual void BeginPlay() override;
+	
+	//  Camera
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent* Camera;
+
+	UFUNCTION()
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Category = "Input")
+	void LookRight(const float Scale);
+
+	UFUNCTION(Category = "Input")
+	void LookUp(const float Scale);
+
+	UFUNCTION(Category = "Input")
+	void MoveForward(const float Scale);
+	
+	UFUNCTION(Category = "Input")
+	void TurnLeft(const float Scale);
+
+	UFUNCTION(Category = "Input")
+	void MoveBack(const float Scale);
+
+	UFUNCTION(Category = "Input")
+	void StartHandBrake();
+
+	UFUNCTION(Category = "Input")
+	void StopHandBrake();
+
+	UFUNCTION(Category = "Input")
+	void GetOutOfBicycle();
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Interfaces")
+	float GetAcceleration() const;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Cyclist")
+	USkeletalMeshComponent* CharacterMesh;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Cyclist")
+	UBoxComponent* GetIntoBoxCollision;
+	
+	UFUNCTION(BlueprintCallable, Category="Cyclist")
+	FBikePoints GetBikePoints() const;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Animation")
+	UAnimMontage* GetOutOfBicycleMontage;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Animation")
+	bool bMirrorAnimations = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup")
+	TSubclassOf<AThirdPersonCharacter> CharacterClass;
+private:
+	UFUNCTION()
+	void OnBoxOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION()
+	void OnBoxOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	bool SideRayCast(const FVector& Direction) const;
+	
+	FTimerHandle GetOutOfBicycleHandle;
+
+	UFUNCTION()
+	void PossessCharacter(const bool Side);
+};
