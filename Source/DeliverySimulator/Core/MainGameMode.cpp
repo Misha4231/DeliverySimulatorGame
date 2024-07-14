716211d7191bbae8bd6ef1b1ac5345e176489dfb
Misha <mishagalko1@gmail.com>
2024-07-14 15:10:22 +0200

    Add main game mode source files

---
+++ Source/DeliverySimulator/Core/MainGameMode.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainGameMode.h"
+
+#include "MainGameInstance.h"
+
+
+void AMainGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UMainGameInstance* GameInstance = Cast<UMainGameInstance>(GetGameInstance());
+
+	if (!GameInstance) return;
+	
+	if (RestaurantCheckpointClass)
+	{
+		TArray<AActor*> FoundRestaurantCheckpoints;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), RestaurantCheckpointClass, FoundRestaurantCheckpoints);
+
+		for (AActor* FoundRestaurantCheckpointActor : FoundRestaurantCheckpoints)
+		{
+			if (const ARestaurantCheckpoint* RestaurantCheckpoint = Cast<ARestaurantCheckpoint>(FoundRestaurantCheckpointActor))
+			{
+				FRestaurant NewRestaurant = FRestaurant();
+			
+				NewRestaurant.Id = RestaurantCheckpoint->GetUniqueID();
+				NewRestaurant.Location = RestaurantCheckpoint->GetActorLocation();
+				NewRestaurant.Name = RestaurantCheckpoint->Name;
+				NewRestaurant.AvailableFoodIds = RestaurantCheckpoint->AvailableFoodIds;
+			
+				GameInstance->Restaurants.Add(NewRestaurant);
+			}
+		}
+	}
+
+	if (DestinationCheckpointClass)
+	{
+		TArray<AActor*> FoundDestinationCheckpoints;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), DestinationCheckpointClass, FoundDestinationCheckpoints);
+
+		for (AActor* FoundDestinationCheckpointActor : FoundDestinationCheckpoints)
+		{
+			if (const ADestinationCheckpoint* DestinationCheckpoint = Cast<ADestinationCheckpoint>(FoundDestinationCheckpointActor))
+			{
+				FDestination NewDestination = FDestination();
+			
+				NewDestination.Id = DestinationCheckpoint->GetUniqueID();
+				NewDestination.Location = DestinationCheckpoint->GetActorLocation();
+				NewDestination.Name = DestinationCheckpoint->Name;
+			
+				GameInstance->Destinations.Add(NewDestination);
+			}
+		}
+	}
+}
---
+++ Source/DeliverySimulator/Core/MainGameMode.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SG_OrdersSlot.h"
+#include "Kismet/GameplayStatics.h"
+#include <DeliverySimulator/Checkpoints/RestaurantCheckpoint.h>
+#include <DeliverySimulator/Checkpoints/DestinationCheckpoint.h>
+#include "MainGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DELIVERYSIMULATOR_API AMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrderData")
+	TSubclassOf<ARestaurantCheckpoint> RestaurantCheckpointClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrderData")
+	TSubclassOf<ADestinationCheckpoint> DestinationCheckpointClass;
+};
