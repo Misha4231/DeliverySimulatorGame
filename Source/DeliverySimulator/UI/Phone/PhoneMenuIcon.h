1a20168681cf817e2b8da1e1b5748477daf6aa7d
Misha <mishagalko1@gmail.com>
2024-07-14 15:21:18 +0200

    add Phone menu icon that going to be in grid on menu screen

---
+++ Source/DeliverySimulator/UI/Phone/PhoneMenuIcon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PhoneMenuIcon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DELIVERYSIMULATOR_API UPhoneMenuIcon : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
