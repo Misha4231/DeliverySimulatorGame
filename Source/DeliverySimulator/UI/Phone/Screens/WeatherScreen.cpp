efd6f23df901bfaef20eeecedbcd60b24eb2e1ab
Misha <mishagalko1@gmail.com>
2024-07-15 20:17:12 +0200

    add weather screen widget

---
+++ Source/DeliverySimulator/UI/Phone/Screens/WeatherScreen.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeatherScreen.h"
+
+void UWeatherScreen::SetScreenChangeDelegate(FScreenChangeDelegate InScreenChangeDelegate)
+{
+	ScreenChangeDelegate = InScreenChangeDelegate;
+}
